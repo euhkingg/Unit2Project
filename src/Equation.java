0324ecefd94a2177c1efbbf4224d08110eb3d4a4
gavin <euhan@macbook-air.myfiosgateway.com>
2023-10-18 23:19:49 -0400

    Created base, fix how coordinates are extracted from string, make sure to follow instructions.

--- src/Equation.java
+++ src/Equation.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Equation {
+import java.awt.Point;
+public class Equation {
+    Point p1;
+    Point p2;
+
+    public Equation(String point1, String point2) {
+        p1 = new Point(Integer.parseInt(point1.substring(1,2)), Integer.parseInt(point1.substring(3,4)));
+        p2 = new Point(Integer.parseInt(point2.substring(1,2)), Integer.parseInt(point2.substring(3,4)));
+    }
+
+    public double distance() {
+        return p1.distance(p2.getX(), p2.getY());
+    }
+
+    public double slope() {
+        return (p2.getY() - p1.getY()) / (p2.getX() - p1.getX());
+    }
+
+    public String yIntercept() {
+        if (p1.getY() - slope() * p1.getX() < 0) {
+            return "- " + Math.abs(p1.getY() - slope() * p1.getX());
+        } else {
+            return "+ " + (p1.getY() - slope() * p1.getX());
+        }
+    }
+
+    public String getP1() {
+        return p1.toString();
+    }
+
+    public String getP2() {
+        return p2.toString();
+    }
 }
--- src/EquationLogic.java
+++ src/EquationLogic.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class EquationLogic {
+import java.util.Scanner;
+import java.awt.Point;
+import java.lang.Integer;
+public class EquationLogic {
+    private Equation line;
+    private Scanner s;
+
+    public EquationLogic() {
+        line = null;
+        s = new Scanner(System.in);
+    }
+
+    public String start() {
+        return "";
+    }
+
+    public void getPoints() {
+        System.out.println("Enter First Point: ");
+        String p1 = s.nextLine();
+        System.out.println("Enter Second Point: ");
+        String p2 = s.nextLine();
+
+        line = new Equation(p1, p2);
+    }
+
+    public String pointInfo() {
+        return "The two points are: " + line.getP1() + " and " + line.getP2() + "\n" +
+                "The equation of the line between these points is: y = " + line.slope() + "x " + line.yIntercept() + "\n" +
+                "The slope of this line is: " + line.slope() +
+                "The y-intercept of this line is: " + line.yIntercept() +
+                "the distance between the tow points is: " + line.distance();
+
+    }
+
+
 }
