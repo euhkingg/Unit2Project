d48cca822b0915ac5d1c7c9da05127544b4870e1
gavin <euhkingg@gmail.com>
2023-10-19 08:33:33 -0400

    base code, need helper methods

--- src/Equation.java
+++ src/Equation.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Equation {
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.lang.Integer;
+public class Equation {
+    Point p1;
+    Point p2;
+
+    public Equation(String point1, String point2) {
+        int x = Integer.parseInt(point1.substring(1, point1.indexOf(",")));
+        int y = Integer.parseInt(point1.substring(point1.indexOf(",") + 1, point1.indexOf(")")));
+        p1 = new Point(x, y);
+        int x2 = Integer.parseInt(point2.substring(1, point2.indexOf(",")));
+        int y2 = Integer.parseInt(point2.substring(point2.indexOf(",") + 1, point2.indexOf(")")));
+        p2 = new Point(x2, y2);
+
+    }
+
+    public String getP1() {
+        return p1.toString();
+    }
+
+    public String getP2() {
+        return p2.toString();
+    }
+
 }
--- src/EquationLogic.java
+++ src/EquationLogic.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class EquationLogic {
+import java.util.Scanner;
+public class EquationLogic {
+    String line;
+    Scanner scan;
+
+    public EquationLogic() {
+        line = null;
+        scan = new Scanner(System.in);
+    }
+
+    public void getPoints() {
+        System.out.println("Enter coordinate 1: ");
+        String p1 = scan.nextLine();
+        System.out.println("Enter coordinate 2: ");
+        String p2 = scan.nextLine();
+
+
+    }
+
+
 }
