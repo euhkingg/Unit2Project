46815350ec586c31a4a2cc616200ef6e95c97b6d
gavin <euhkingg@gmail.com>
2023-10-19 09:34:09 -0400

    Added more methods

--- src/Equation.java
+++ src/Equation.java
@@ -1,9 +1,11 @@
 import java.awt.Point;
-import java.awt.geom.Point2D;
+import java.io.*;
 import java.lang.Integer;
 public class Equation {
     Point p1;
     Point p2;
+    double slope;
+
 
     public Equation(String point1, String point2) {
         int x = Integer.parseInt(point1.substring(1, point1.indexOf(",")));
@@ -12,6 +14,7 @@ public class Equation {
         int x2 = Integer.parseInt(point2.substring(1, point2.indexOf(",")));
         int y2 = Integer.parseInt(point2.substring(point2.indexOf(",") + 1, point2.indexOf(")")));
         p2 = new Point(x2, y2);
+        slope = 0;
 
     }
 
@@ -21,6 +24,37 @@ public class Equation {
 
     public String getP2() {
         return p2.toString();
+    }
+
+    public double distance() {
+        return p1.distance(p2.getX(), p2.getY());
+    }
+
+    public String slope() {
+        slope = (p2.getY() - p1.getY()) / (p2.getX() - p1.getX());
+        if (slope == 1) {
+            return "";
+        } else if (slope == -1) {
+            return "-";
+        } else if (slope % 1.0 == 0) {
+            return String.valueOf(slope);
+        } else if (p2.getY() - p1.getY() < 0 && p2.getX() - p1.getX() < 0) {
+            return Math.abs((p2.getY() - p1.getY())) + "/" + Math.abs((p2.getX() - p1.getX()));
+        } else if (p2.getY() - p1.getY() > 0 && p2.getX() - p1.getX() < 0) {
+            return "-" + (p2.getY() - p1.getY()) + "/" + (p2.getX() - p1.getX());
+        }
+        return (p2.getY() - p1.getY()) + "/" + (p2.getX() - p1.getX());
+    }
+
+    public String yIntercept() {
+        double intercept  = p2.getY() - slope * p1.getX();
+        if (intercept < 0) {
+            return " - " + Math.abs(intercept);
+        } else if (intercept == 0) {
+            return "";
+        } else {    
+            return " + " + intercept;
+        }
     }
 
 }
--- src/EquationLogic.java
+++ src/EquationLogic.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 public class EquationLogic {
-    String line;
+    Equation line;
     Scanner scan;
 
     public EquationLogic() {
         line = null;
         scan = new Scanner(System.in);
+    }
+
+    public void Start() {
+        int x;
     }
 
     public void getPoints() {
@@ -14,8 +18,19 @@ public class EquationLogic {
         System.out.println("Enter coordinate 2: ");
         String p2 = scan.nextLine();
 
+        line = new Equation(p1, p2);
+    }
+
+    public String pointinfo() {
+        return "The two poitns are: " + line.getP1() + " and " + line.getP2() + "\n" +
+                "The equation of the line between these points is: y = " + line.slope() + "x " + line.yIntercept() + "\n" +
+                "The slope of this line is: " + line.slope() + "\n" +
+                "The y-intercept of the line is: " + line.yIntercept() + "\n" +
+                "The distance between the two points is: " + line.distance();
 
     }
+
+
 
 
 }
